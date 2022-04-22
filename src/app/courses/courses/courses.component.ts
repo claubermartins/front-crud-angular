d39dc475878ff8b189f24bc9849f01c602323326
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-04-22 17:19:30 -0300

    Iniciando a criação do formulário

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable, of } from 'rxjs';
@@ -15,17 +16,19 @@ import { CoursesService } from './../services/courses.service';
 export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]>;
-  displayedColumns = ['name','category'];
+  displayedColumns = ['_id', 'name', 'category', 'email', 'actions'];
 
   //coursesService: CoursesService;
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
-    ) {
-      //this.coursesService = new CoursesService();
-      //this.courses = this.coursesService.list();
-      this.courses$ = this.coursesService.list()
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    //this.coursesService = new CoursesService();
+    //this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
       .pipe(catchError(error => {
         this.onError('Erro ao carregar cursos.');
         return of([])
@@ -40,6 +43,10 @@ export class CoursesComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
 }
